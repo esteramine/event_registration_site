859048dcc1b5b5a0aeacf58bc0c489df0b53a088
Ann <cchouaa@connect.ust.hk>
2022-01-07 20:13:57 +0800

    [client] client init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline flex justify-center items-center h-screen">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
