c686e8b90256f72abc55d204242d2dc40e0410d6
Ann <cchouaa@connect.ust.hk>
2022-01-08 22:47:37 +0800

    [client] LoginCard linked to login api

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -1,25 +1,69 @@
+import gql from "graphql-tag";
+import { useMutation } from '@apollo/client';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useForm } from '../utils/hooks';
+
 function LoginCard() {
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+
+    const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+        userId: '',
+        password: '',
+    });
+
+    const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+        update(_, result) {
+            navigate('/');
+        },
+        onError(err) {
+            console.log(err.graphQLErrors[0].extensions.exception);
+            setErrors(err.graphQLErrors[0].extensions.exception.errors);
+        },
+        variables: values
+    });
+
+    function loginUserCallback() {
+        loginUser();
+    }
+
+
     return (
         <div className="bg-white w-[550px] p-4 m-2 rounded-md flex flex-col space-y-2 text-gray-700 shadow-lg">
             <header className="font-bold text-3xl flex items-center justify-center py-2">Login</header>
+
+            <>
+                {Object.keys(errors).length > 0 && (
+
+                    <div className="py-2 px-8 bg-red-100 text-red-600 text-sm rounded-md">
+                        <ul>
+                            {Object.values(errors).map(value => (
+                                <li className="list-disc" key={value}>{value}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </>
 
             <div className="flex flex-col space-x-1">
                 <div className="flex space-x-2 p-2">
                     <div className="font-bold font-sm">USER ID</div>
                 </div>
-                <input className="register-input" />
+                <input name="userId" type="text" className={errors.userId ? "register-input-error" : "register-input"} onChange={onChange} />
             </div>
 
             <div className="flex flex-col space-x-1">
                 <div className="flex space-x-2 p-2">
                     <div className="font-bold font-sm">PASSWORD</div>
                 </div>
-                <input className="register-input" />
+                <input name="password" type="password" className={errors.password ? "register-input-error" : "register-input"} onChange={onChange} />
             </div>
 
 
             <div className="pt-2">
-                <div className="register-button">
+                <div className="register-button" onClick={onSubmit}>
                     LOGIN
                 </div>
 
@@ -32,5 +76,25 @@ function LoginCard() {
         </div>
     )
 }
+
+const LOGIN_USER = gql`
+    mutation login(
+        $userId: String!
+        $password: String!
+    ) {
+        login(
+            loginInput: {
+                userId: $userId
+                password: $password
+            }
+        ) {
+            id
+            userId
+            name
+            createdAt
+            token
+        }
+    }
+`
 
 export default LoginCard;
